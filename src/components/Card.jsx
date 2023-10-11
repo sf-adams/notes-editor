7895de689f8e3bd1ffeaf8d76531a28e19a144cc
Sam <samuel.fitz@btinternet.com>
2023-10-11 20:22:07 +0100

    feat: add all content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,22 @@
+import pin from "../assets/pin.png";
+
 function Card(props) {
   console.log(props);
-  return <div>
-    {props.card.title}
-  </div>;
+  return (
+    <article className="post">
+      <img src={props.card.imageUrl} alt="" />
+      <div className="post__wrapper">
+        <div className="post__location">
+          <img src={pin} alt="Pin" />
+          <p>{props.card.location}</p>
+          <a href="">View on Google Maps</a>
+        </div>
+        <h2>{props.card.title}</h2>
+        <p>{props.card.startDate} - {props.card.endDate}</p>
+        <p>{props.card.description}</p>
+      </div>
+    </article>
+  );
 }
 
 export default Card;
