f161748ed39fff718acc46d1bf5ab7c643c41c70
Sam <samuel.fitz@btinternet.com>
2023-10-30 13:32:11 +0000

    feat: add note functions and basic editor features

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,44 @@ import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Sidebar from "./components/Sidebar";
+import { nanoid } from "nanoid";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
+  );
 
-  function createNewNote() {
-    const newNote = {};
+  function createNewNote(text) {
+    const newNote = {
+      id: nanoid(),
+      body: `${text}`,
+    };
+    setNotes((prevNotes) => {
+      return [newNote, ...prevNotes];
+    });
+    setCurrentNoteId(newNote.id);
   }
+
+  function updateNote() {}
+
+  function findCurrentNote() {
+    console.log("This runs")
+    return notes.find((note) => {
+      return note.id === currentNoteId
+
+    });
+  }
+
+  console.log(notes)
 
   return (
     <main>
       {notes.length > 0 ? (
         <div>
-          <Sidebar />
-          <Editor />
+          <button onClick={createNewNote}>Create one now</button>
+          {/* <Sidebar findCurrentNote={findCurrentNote} /> */}
+          <Editor currentNote={findCurrentNote()}/>
         </div>
       ) : (
         <div>
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,39 @@
-const Editor = () => {
-  return <div>Editor</div>;
-};
+import ReactMde from "react-mde";
+import Showdown from "showdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import React from "react";
 
-export default Editor;
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true,
+});
+
+export default function Editor({ currentNote }) {
+  const [selectedTab, setSelectedTab] = React.useState("write")
+
+  // const converter = new Showdown.Converter({
+  //     tables: true,
+  //     simplifiedAutoLink: true,
+  //     strikethrough: true,
+  //     tasklists: true,
+  // })  
+
+  return (
+      <section className="pane editor">
+        {currentNote}
+          {/* <ReactMde
+              value={currentNote.body}
+              onChange={updateNote}
+              selectedTab={selectedTab}
+              onTabChange={setSelectedTab}
+              generateMarkdownPreview={(markdown) =>
+                  Promise.resolve(converter.makeHtml(markdown))
+              }
+              minEditorHeight={80}
+              heightUnits="vh"
+          /> */}
+      </section>
+  )
+}
